454606bcb67ec4696fefe1fab871ef3d34c1cc2e
jacquelinesantana <ti.jacque@gmail.com>
2025-05-21 23:32:22 -0300

    :sparkles: feat: Add Put for items by order

--- src/main/java/com/lanchonete/items/controller/ItemController.java
+++ src/main/java/com/lanchonete/items/controller/ItemController.java
@@ -3,14 +3,19 @@ package com.lanchonete.items.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lanchonete.items.model.OrderItems;
 import com.lanchonete.items.repository.ItemRepository;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -26,5 +31,13 @@ public class ItemController {
 		return ResponseEntity.ok(iRepository.findAll());
 	}
 	
-	
+	@PutMapping
+	public ResponseEntity<OrderItems> put(@RequestBody @Valid OrderItems oItems){
+		if(iRepository.existsById(oItems.getId())) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(iRepository.save(oItems));
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+		
 }
